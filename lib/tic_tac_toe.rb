49feb85d2979ca41869545499d1b469387fd2d0f
kovner chalumo <kchalumo@gmail.com>
2018-12-17 01:50:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,6 +23,9 @@ class TicTacToe
     @board[index] == " " || @board[index] == "" || @board[index] == nil ? false : true 
   end
   
+  def valid_move?(index)
+    index.between?(0, 8) && !position_taken(index) ? true : false
+  end
   
   
 end
