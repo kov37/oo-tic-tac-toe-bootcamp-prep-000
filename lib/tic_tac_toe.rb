eda56a9bc23a3e49053d5788dbc88f2438f84f59
kovner chalumo <kchalumo@gmail.com>
2018-12-17 02:09:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ class TicTacToe
   end
   
   def valid_move?(index)
-    index.between?(0, 8) && !position_taken(index) ? true : false
+    index.between?(0, 8) && !position_taken?(index) ? true : false
   end
   
   def turn_count
